1c9a074ed4a6f70697ee803e3d523a8aee1a4126
tokizuoh <tokizuoh@gmail.com>
2023-02-12 13:11:07 +0900

    init ios project

---
+++ MonthlyDistance/MonthlyDistanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyDistanceApp.swift
+//  MonthlyDistance
+//
+//  Created by tokizo on 2023/02/12.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyDistanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
