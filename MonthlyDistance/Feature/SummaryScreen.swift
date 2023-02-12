6a61357591a33b65ea38f88208378fc90e99d2e5
tokizuoh <tokizuoh@gmail.com>
2023-02-12 13:45:51 +0900

    add SummaryScreen && remove ContentView

---
+++ MonthlyDistance/Feature/SummaryScreen.swift
@@ -0,0 +1,20 @@
+//
+//  SummaryScreen.swift
+//  MonthlyDistance
+//
+//  Created by tokizo on 2023/02/12.
+//
+
+import SwiftUI
+
+struct SummaryScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SummaryScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SummaryScreen()
+    }
+}
