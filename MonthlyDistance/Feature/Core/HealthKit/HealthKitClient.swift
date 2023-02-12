1dc8457ab4c20ca44dc3e32fe33521560e311ba3
tokizuoh <tokizuoh@gmail.com>
2023-02-12 15:26:32 +0900

    add fetchHealthKitData()

--- MonthlyDistance/Feature/Core/HealthKit/HealthKitClient.swift
+++ MonthlyDistance/Feature/Core/HealthKit/HealthKitClient.swift
@@ -7,6 +7,17 @@
 
 import HealthKit
 
+enum HealthKitDataType {
+    enum Workout {
+        case walking
+        case running
+        case cycling
+    }
+
+    case distanceWalkingRunning
+    case workout(Workout)
+}
+
 final class HealthKitClient {
     static let shared =  HealthKitClient()
 
@@ -21,5 +32,24 @@ final class HealthKitClient {
         ])
 
         try await healthStore.requestAuthorization(toShare: Set([]), read: readTypes)
+    }
+
+    func fetchData(for type: HealthKitDataType, from: Date, to: Date) async throws -> [HKSample]? {
+        guard case .distanceWalkingRunning = type else {
+            return nil
+        }
+
+        let descriptor = HKSampleQueryDescriptor(
+            predicates: [
+                .sample(
+                    type: HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.distanceWalkingRunning)!,
+                    predicate: HKQuery.predicateForSamples(withStart: from, end: to, options: [])
+                )
+            ],
+            sortDescriptors: []
+        )
+
+        let samples = try await descriptor.result(for: healthStore)
+        return samples
     }
 }
--- MonthlyDistance/Feature/Summary/SummaryViewModel.swift
+++ MonthlyDistance/Feature/Summary/SummaryViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by tokizo on 2023/02/12.
 //
 
-import Foundation
+import HealthKit
 
 final class SummaryViewModel: ObservableObject {
     let healthKitClient: HealthKitClient
@@ -16,6 +16,7 @@ final class SummaryViewModel: ObservableObject {
 
     func onAppear() async {
         await setupHealthKit()
+        await fetchHealthKitData()
     }
 
     private func setupHealthKit() async {
@@ -23,6 +24,18 @@ final class SummaryViewModel: ObservableObject {
             try await healthKitClient.requestAuthorization()
         } catch {
             // TODO: Handle Error
+        }
+    }
+
+    private func fetchHealthKitData() async -> [HKSample]? {
+        do {
+            let now = Date()
+            let startOfMonth = Calendar.current.date(from: Calendar.current.dateComponents([.year, .month], from: Calendar.current.startOfDay(for: now)))!
+            let healthKitData = try await healthKitClient.fetchData(for: .distanceWalkingRunning, from: startOfMonth, to: now)
+            return healthKitData
+        } catch {
+            // TODO: Handle Error
+            fatalError()
         }
     }
 }
